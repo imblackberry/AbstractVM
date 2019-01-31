cc4dcccf7df66d460db79b405b627030aab8c4a3
imblackbarry <blokhavictoria@gmail.com>
2019-01-31 16:59:49 +0200

    result Operand<T> + Operand<T> is IOperand

--- headers/Operand.hpp
+++ headers/Operand.hpp
@@ -1,6 +1,7 @@
 #ifndef OPERAND_HPP
 #define OPERAND_HPP
 #include "IOperand.hpp"
+#include <boost/lexical_cast.hpp>
 #define BITS_IN_BYTE 8
 template <class T>
 class Operand : public IOperand{
@@ -12,8 +13,8 @@ class Operand : public IOperand{
 		// Operand(const Operand & other);
 		Operand const & operator=(Operand const & other){
 			if (this != &other){
-				_value = other._value;
 				_strValue = other._strValue;
+				_value = other._value;
 			}
 			return *this;
 		}
@@ -21,18 +22,23 @@ class Operand : public IOperand{
 		T const & getValue() const {return _value;}
 		// virtual int					getPrecision(void) const;	// Precision of the type of the instance
 		// virtual	eOperandType		getType(void) const;	// Type of the instance
-		// virtual Operand const *	operator+(IOperand const & rhs) const; // Sum
+		IOperand const *	operator+(IOperand const & rhs) const {
+			IOperand const *tmp = new Operand (boost::lexical_cast<std::string>(_value + static_cast<T>(std::stod(rhs.toString()))));
+			return tmp;
+		} // Sum
 		// virtual IOperand const *	operator-(IOperand const & rhs) const; // Difference
 		// virtual IOperand const *	operator*(IOperand const & rhs) const; // Product
 		// virtual IOperand const *	operator/(IOperand const & rhs) const; // Quotient
 		// virtual IOperand const *	operator%(IOperand const & rhs) const; // Modulo
-		virtual std::string const & toString( void ) const{return _strValue;} // String representation of the instance
+		virtual std::string const & toString( void ) const {
+			return _strValue;
+		} // String representation of the instance
 	private:
 		T				_value;
 		std::string		_strValue;
 };
 // ------------------------------
-// 			DECLARATION
+// 			5 CLASS DECLARATION
 // ------------------------------
 template class Operand<int8_t>;
 template class Operand<int16_t>;
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,12 +1,22 @@
 #include "Operand.hpp"
-int main(){
-	
-	Operand<double> a("45.65600001");
-	// Operand<int32_t> b(8);
-	// Operand<int32_t> c;
+template< class T>
+Operand<T> * sum(Operand<T> * a, Operand<T> *b){
+	Operand<T> * c = new Operand<T>("7");
+	a + b;
+	(void)a;
+	(void)b;
 
-	// c = a + b;
-	std::cout << a.getValue() << std::endl; 
-	// std::cout << b.toString() << std::endl; 
-	// std::cout << c.toString() << std::endl; 
+	return c;
+}
+int main(){
+
+	Operand<double> *a = new Operand<double>("45.65600001");
+	Operand<double> *b = new Operand<double>("1");
+	IOperand const *c = *a + *b;
+
+	// c = sum<double>(a, b);
+	// c = *a + *b;
+	std::cout << a->toString() << std::endl; 
+	std::cout << b->toString() << std::endl; 
+	std::cout << c->toString() << std::endl; 
 }
