c4a4244a21a828ca274ff33703ed8abb9c62175d
imblackbarry <blokhavictoria@gmail.com>
2019-02-02 18:32:04 +0200

    simple factory

--- headers/OperandFactory.hpp
+++ headers/OperandFactory.hpp
@@ -13,12 +13,12 @@ class OperandFactory{
 	
 	private:
 		IOperand const * createInt8(std::string const & value) const;
-		// IOperand const * createInt16(std::string const & value) const;
-		// IOperand const * createInt32(std::string const & value) const;
-		// IOperand const * createFloat(std::string const & value) const;
-		// IOperand const * createDouble(std::string const & value) const;
+		IOperand const * createInt16(std::string const & value) const;
+		IOperand const * createInt32(std::string const & value) const;
+		IOperand const * createFloat(std::string const & value) const;
+		IOperand const * createDouble(std::string const & value) const;
 
-		// IOperand const * (OperandFactory::*factory[5])(std::string const & value) const;
+		std::vector<IOperand const * (OperandFactory::*)(std::string const &) const > factory;
 };
 
 #endif
--- sources/OperandFactory.cpp
+++ sources/OperandFactory.cpp
@@ -1,6 +1,6 @@
 #include "OperandFactory.hpp"
 OperandFactory::OperandFactory(){
-	factory[0] = &OperandFactory::createInt8;
+	factory.push_back(&OperandFactory::createInt8);
 }
 
 // OperandFactory::OperandFactory(const OperandFactory & other){
@@ -17,25 +17,24 @@ OperandFactory::~OperandFactory(){
 }
 
 IOperand const * OperandFactory::createOperand(eOperandType type, std::string const & value ) const{
-	return factory[0](value);
+	return (this->*factory[type])(value);
 }
 
 IOperand const * OperandFactory::createInt8(std::string const & value) const{
 	return new Operand<int8_t>(value);
 }
-// IOperand const * OperandFactory::createInt16(std::string const & value) const{
-// 	return new Int(value);
+IOperand const * OperandFactory::createInt16(std::string const & value) const{
+	return new Operand<int16_t>(value);
 
-// }
-// IOperand const * OperandFactory::createInt32(std::string const & value) const{
-// 	return new Int(value);
+}
+IOperand const * OperandFactory::createInt32(std::string const & value) const{
+	return new Operand<int32_t>(value);
 
-// }
-// IOperand const * OperandFactory::createFloat(std::string const & value) const{
-// 	IOperand  *tmp = new Operand<float, 32>(std::stof(value));
-// 	return tmp;
-// }
+}
+IOperand const * OperandFactory::createFloat(std::string const & value) const{
+	return new Operand<float>(value);
+}
 
-// IOperand const * OperandFactory::createDouble(std::string const & value) const{
-// 	return new Operand<double, 64>(std::stod(value));
-// }
+IOperand const * OperandFactory::createDouble(std::string const & value) const{
+	return new Operand<double>(value);
+}
